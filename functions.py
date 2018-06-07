1b21901696e7ecf443f9317d86898a592f8e6d7a
Antonio-Djigo <antonio@secret-source.eu>
2018-06-07 12:52:41 +0100

    Can add users

--- functions.py
+++ functions.py
@@ -15,11 +15,29 @@ def print_students_titleclase():
     students_titlecase = get_students_titlecase()
     print(students_titlecase)   
 
-answer = None
+def save_into_file(student):
+    try:
+        f = open("./students.txt", "a")
+        f.write(student + "\n")
+        f.close()
+    except Exception:
+        print ("Can not save the file")
+
+def read_file():
+    try:
+        f = open("students.txt", "r")
+        for student in f.readlines():
+            print (student)
+        f.close()
+    except Exception:
+        print ("Can not read the file")
+
+read_file()
 while True:
-    student_name = input("Enter student name, or say No to exit")
+    student_name = input("Enter student name, or say No to exit :")
     if student_name.lower() == "no":
+        print("Bye!")
         break
     student_id = input("Enter student id: ")
     add_student(student_name, student_id)
-    print_students_titleclase()
+    save_into_file(student_name)
